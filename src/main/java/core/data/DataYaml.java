6a03a0f029e40c3669a45cdaf6453a43f01c4ae4
Samuel Leite <samuel-leite@outlook.com.br>
2025-12-04 20:40:58 -0300

    inclusao url separada por ambiente

--- src/main/java/core/data/DataYaml.java
+++ src/main/java/core/data/DataYaml.java
@@ -13,9 +13,15 @@ import java.util.Map;
 @Log4j2
 public class DataYaml {
 
+    private static File getYamlConfFile(){
+        String env = System.getProperty("env"); // default qa
+        log.info("Carregando a URL do ambiente de '{}'", env);
+        return new File("./src/main/resources/conf/url-" + env + ".yml");
+    }
+
     private static File getYamlDataFile(String fileName){
         String env = System.getProperty("env");
-        log.info("Pegando arquivo {}.yml do ambiente {}", fileName, env);
+        log.info("Carregando a massa de teste do ambiente de '{}'", env);
         return new File("./src/test/resources/data/" + env + "/" + fileName + ".yml");
     }
 
@@ -25,11 +31,20 @@ public class DataYaml {
         mapper.findAndRegisterModules();
         try {
             Map<String , Object> maps = mapper.readValue(getYamlDataFile(fileName), Map.class);
-            log.info("Retornando objeto HashMap com massa de dados do arquivo {} com titulo {}", fileName, titulo);
+            log.info("Retornando objeto HashMap com massa de dados do arquivo '{}' com titulo '{}'", fileName, titulo);
             return (LinkedHashMap<String, String>) maps.get(titulo);
         } catch (IOException e) {
             log.error("Erro ao tentar ler o arquivo de massa {}.yml - {}", fileName, e.getMessage(), e);
             throw new Exception(e);
         }
+    }
+
+    @SneakyThrows
+    public static String getUrlBase() {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.findAndRegisterModules();
+
+        Map<String, Object> maps = mapper.readValue(getYamlConfFile(), Map.class);
+        return (String) maps.get("urlBase");
     }
 }
