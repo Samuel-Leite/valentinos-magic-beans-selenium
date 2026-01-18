3ce67c25c6da9e4351619e571c17e2a4b3605dbe
Samuel Leite <samuel-leite@outlook.com.br>
2026-01-18 19:09:15 -0300

    correcao codigo

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -3,19 +3,17 @@ package steps;
 import core.data.DataYaml;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Quando;
-import lombok.extern.log4j.Log4j2;
 import pages.LoginPage;
 
 import java.util.LinkedHashMap;
 
-@Log4j2
 public class LoginSteps {
 
     LoginPage loginPage = new LoginPage();
 
-    @Dado("^que acesse a pagina de login$")
+    @Dado("que acesse a pagina de login")
     public void queAcesseAPaginaDeLogin() {
-        loginPage.acessaAplicacao();
+        loginPage.acessarAplicacao();
     }
 
     @Quando("informar as credenciais {string}")
